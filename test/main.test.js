33f44e9f8fdd1c1e9fc4c58e1d6bb47bc16e1095
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2020-09-25 18:27:05 -0500

    Update main.test.js

--- test/main.test.js
+++ test/main.test.js
@@ -1,11 +1,18 @@
-QUnit.module('MAIN MODULE', {})
-
-
-QUnit.test('TEST CalcInterest()', assert => {
-  document.querySelector('#pr').value=100
-  document.querySelector('#te').value=1
-  document.querySelector('#rate').value=10
-  assert.equal(CalcInterest(),100,1,10,"equlas 110")
-  assert.strictEqual(input.checkValidity(), false, 'Correctly fails validation')
- })
-
+const mul = (a, b) => a * b;
+QUnit.module('mul', function() {
+  QUnit.test('should add two numbers', function(assert) {
+    assert.equal(mul(1, 1), 1, '1 * 1 = 1');
+  });
+    QUnit.test('should add two numbers', function(assert) {
+    assert.equal(mul(2, 2), 4, '2 * 2 = 4');
+  });
+    QUnit.test('should add two numbers', function(assert) {
+    assert.equal(mul(3, 3), 9, '3 * 3 = 9');
+  });
+    QUnit.test('should add two numbers', function(assert) {
+    assert.equal(mul(4, 4), 16, '4 * 4 = 16');
+  });
+    QUnit.test('should add two numbers', function(assert) {
+    assert.equal(mul(5, 5), 25, '5 * 5 = 25');
+  });
+});
